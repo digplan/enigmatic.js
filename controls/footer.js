8d076d81a50487b547e6580a444aa7abac48b7c4
digplan <info@dpsw.info>
2015-01-02 18:51:52 -0600

    fix

--- controls/footer.js
+++
@@ -1,7 +0,0 @@
-function footer(){
-  var items = this.attr('items').split(' ');
-  for(var i=0; i<items.length; i++){
-    this.child(items[i++], items[i]=='#' ? 'span':'a', {href: items[i]}); 
-  }
-}
-  // body.child('', '', {items:'terms /terms privacy /privacy &#9731; # api /api contact /contact', control:'footer'});
