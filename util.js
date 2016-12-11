c687a038c3f0b471392430ae68bc14a8bd10f4c4
digital planets <digplan@users.noreply.github.com>
2016-12-10 20:46:17 -0600

    Create util.js

---
+++ util.js
@@ -0,0 +1,10 @@
+enig.util = {};
+enig.util.twodarray(arr) {
+  var keys=arr.shift();
+  return arr.map((i)=>{
+    var o={};
+    for(var j=0;j<keys.length;j++)
+      o[keys[j]] = i[j];
+    return o;
+  });
+}
