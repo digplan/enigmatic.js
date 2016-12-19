3f6e48fcc1a7c5af8b743a99ff52ba2adb4077e2
digital planets <digplan@users.noreply.github.com>
2016-12-19 12:13:22 -0600

    Update places.js

--- controls/places.js
+++ controls/places.js
@@ -7,6 +7,10 @@ enig.places = (e)=>{
   enig.load(`https://maps.googleapis.com/maps/api/js?key=${e.getAttribute('api-key')}&libraries=places`, ()=>{
     var input = e.child('input');
     input.id = 'placecontrol';
+    input.style.width = '100%';
+    input.style.height = '100%';
+    input.onclick = () => input.select();
+    input.onkeyup = (ev) => { if(ev.keyCode == 27) input.value = ''; }
     var ac = new google.maps.places.Autocomplete(input, {types: ['geocode']});
     ac.addListener('place_changed', () => e.onselected(ac.getPlace()));
   })
