8ea6b9c00be6a0b0cb6dba15817ba1ca7bee2b8d
digital planets <digplan@users.noreply.github.com>
2016-12-21 00:55:18 -0600

    Update places.js

--- controls/places.js
+++ controls/places.js
@@ -2,15 +2,23 @@
   <places id=myplaces api-key=KEY control></places>
   myplaces.onselected = (e) => alert(JSON.stringify(e));
 */
-enig.places = (e)=>{
-  enig.places_cb = () => {
-    var input = e.child('input');
-    input.style.width = '100%';
-    input.style.height = '100%';
-    input.onclick = () => input.select();
-    input.onkeyup = (ev) => { if(ev.keyCode == 27) input.value = ''; }
-    var ac = new google.maps.places.Autocomplete(input, {types: ['geocode']});
-    ac.addListener('place_changed', () => e.onselected(ac.getPlace()));
+
+enig.places_cb = () => {
+  var e = enig.places.count.shift();
+  var input = e.child('input');
+  input.style.width = '100%';
+  input.style.height = '100%';
+  input.onclick = () => input.select();
+  input.onkeyup = (ev) => {
+    if (ev.keyCode == 27) input.value = '';
   }
+  var ac = new google.maps.places.Autocomplete(input, { types: ['geocode'] });
+  ac.addListener('place_changed', () => e.onselected(ac.getPlace()));
+  if (enig.places.count.length) enig.places_cb();
+}
+
+enig.places = (e) => {
   enig.load(`https://maps.googleapis.com/maps/api/js?key=${e.getAttribute('api-key')}&libraries=places&callback=enig.places_cb`);
+  if (!enig.places.count) enig.places.count = [];
+  enig.places.count.push(e);
 }
