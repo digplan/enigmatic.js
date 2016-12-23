849fbf0206f036b492f614a6eb521f452d025560
digital planets <digplan@users.noreply.github.com>
2016-12-23 17:19:15 -0600

    Update router.js

--- controls/router.js
+++ controls/router.js
@@ -1,20 +1,6 @@
 enig.router = (e) => {
-    var home = location.href;
-    for(var ls = document.links, numLinks = ls.length, i=0; i<numLinks; i++){
-      // Clicking a link will push state
-      ls[i].onclick= (function(linktarget){
-         return function(){
-           var path = linktarget.split('/').pop();
-           history.pushState({}, '', path);
-           // Hide views other than selected
-           $('router > view').forEach((view)=>{
-             view.style.display = view.id == path ? '' : 'none';
-             view.hidden = false;
-           });
-           //if(window._onviewchange) window._onviewchange(path);
-           return false;
-         };
-         // link target
-      })(ls[i].href);
-    }
+  e.route = (name) => {
+    history.pushState({}, '', name);
+    $('router > view').forEach((v)=>{v.hidden = v.id==name});
+  }
 };
