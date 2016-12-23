a493c69379297b42b37cd9bba88ed07b13cda346
digital planets <digplan@users.noreply.github.com>
2016-12-23 17:33:16 -0600

    Update router.js

--- controls/router.js
+++ controls/router.js
@@ -1,6 +1,12 @@
+/*
+  document.onlogin = () => {
+    myrouter.route('home');
+  };
+*/
+
 enig.router = (e) => {
   e.route = (name) => {
     history.pushState({}, '', name);
-    $('router > view').forEach((v)=>{v.hidden = v.id==name});
+    $('router > view').forEach((v)=>{v.hidden = v.id != name});
   }
 };
