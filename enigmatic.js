a9bb0b886f6f33b48de996423380b093cf156108
digital planets <digplan@users.noreply.github.com>
2016-12-30 14:49:37 -0600

    Update enigmatic.js

--- enigmatic.js
+++ enigmatic.js
@@ -58,7 +58,13 @@
     set: function(target, property, value, receiver) {
       $(`[data=${property}]`).forEach(function(e){
          var v = value;
-         if(e.getAttribute('field')) v = v[e.getAttribute('field')];
+         var prop = e.getAttribute('data-property');
+         if(prop){
+           prop = prop.split('.');
+           prop.forEach((k)=>{
+              v = v[k];
+           });
+         }
          if(e.setValue) e.setValue(v);
          else if(typeof e.value != 'undefined') e.value = v;
          else if(typeof e.innerHTML != 'undefined') e.innerHTML = v;
