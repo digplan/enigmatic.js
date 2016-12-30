b83f060b206d299e195e15f377f8dd8fa0a30a68
digital planets <digplan@users.noreply.github.com>
2016-12-30 12:53:24 -0600

    Create panel.js

---
+++ controls/panel.js
@@ -0,0 +1,20 @@
+enig.panel = (e) => {
+  var templ = e.innerHTML;
+
+  function arr(data){
+    data.forEach((row)=>{
+      var child = e.child();
+      child.innerHTML = enig.format(templ, row);
+    });
+  }
+  
+  e.setValue = (data) => {
+    console.log(data)
+    e.innerHTML = '';
+    if(!data.push)
+      e.innerHTML = enig.format(templ, data);
+    else
+      arr(data);
+    e.hidden = false;
+  }
+}
