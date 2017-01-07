0449b33a99140ef51f7eece9f009ae095ca1eb3c
digital planets <digplan@users.noreply.github.com>
2017-01-07 15:08:58 -0600

    Update colorPalette.js

--- controls/colorPalette.js
+++ controls/colorPalette.js
@@ -1,25 +1,16 @@
-enig.colorPalette = (e) => {
-  e.setValue = (arr) => {
-    e.style.backgroundColor = 'black';
-    e.style.position = 'fixed';
-    e.style.width = '300px';
-    e.style.height = '200px';
-    var len = arr.length;
-    var child_width = e.offsetWidth/len;
-    while(len--){
-      var child = e.child();
-      var val = arr[len];
-      if(val[0] != '#') val = '#' + val;
-      child.style.backgroundColor = val;
-      child.style.width = child_width+'px';
-      child.style.height = '100%';
-      child.style.float = 'left';
-    }
+enig['color-palette'] = (e) => {
+  e.css('background-color:#dbdbdb;position:fixed');
+  e.render = arr => {
+    var width = e.offsetWidth/arr.length;
+    e.innerHTML = arr.map(color =>
+      `<div style='float:left;height:100%;width:${width};background-color:${color}'>&nbsp;</div>`
+    ).join('');
   }
-}
+} 
 
 /*
-<colorPalette id=me control></colorPalette>
-enig.colorPalette(element);
-element.setValue(["52433E", "BF402F", "D47944", "FFC486", "FFF7B3", "cbcbcb"]);
+<color-palette class='eight eight-high' id=my data=colors control>
+
+enig.ready = k =>
+  data.colors = ["52433E", "BF402F", "D47944", "FFC486", "FFF7B3", "cbcbcb"]
 */
