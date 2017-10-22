272c4632901ab367a8bdec9297a560276fab05e6
digital planets <digplan@users.noreply.github.com>
2017-10-21 21:50:22 -0500

    Update enigmatic.js

--- enigmatic.js
+++ enigmatic.js
@@ -1,32 +1,33 @@
-window.onload = function(){
-try {
+enigmatic = x => {
+ try {
  window.erun = function (){
- data = new Proxy({}, {
-  set: function(target, property, value, receiver) {
-   document.querySelectorAll(`[data=${property}]`).forEach(function(e){
-    var v = value;
-    var prop = e.getAttribute('data-property');
-    if(prop) v = eval('value.' + prop);
-    if(e.set) e.set(v);
-    if(e.render) e.render(v);
-  });
-      target[property] = value;
+   window.data = new Proxy({}, {
+    set: function(target, property, value, receiver) {
+     document.querySelectorAll(`[data=${property}]`).forEach(function(e){
+      var v = value;
+      var prop = e.getAttribute('data-property');
+      if(prop) v = eval('value.' + prop);
+      if(e.set) e.set(v);
+      if(e.render) e.render(v);
+    });
+        target[property] = value;
+      }
+   });
+   document.querySelectorAll('[control]').forEach((e)=>{
+    console.log(e)
+    var ename = e.tagName.toLowerCase();
+    var cn = e.getAttribute('control');
+    if(cn) ename = cn;
+    e.css = (rules, sel)=>{
+      var style = document.createElement("style");
+      document.head.appendChild(style);
+      style.sheet.insertRule(`${sel||e.tagName} { ${rules} }`); 
     }
-  });
- document.querySelectorAll('[control]').forEach((e)=>{
-  console.log(e)
-  var ename = e.tagName.toLowerCase();
-  var cn = e.getAttribute('control');
-  if(cn) ename = cn;
-  e.css = (rules, sel)=>{
-    var style = document.createElement("style");
-    document.head.appendChild(style);
-    style.sheet.insertRule(`${sel||e.tagName} { ${rules} }`); 
-  }
-  window[ename](e);
- });
+    window[ename](e);
+   });
  }
-} catch(e) {
+ } catch(e) {
    document.body.innerHTML = `<div style='color:red'>${e.stack}</div>`
-}  
+ }  
 }
+window.onload = enigmatic
