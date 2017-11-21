83ca5911d5a3c7882ddd5361256e34ca68e6b1fe
C Borkert <borkert@hotmail.com>
2017-11-21 16:13:40 -0600

    fix

--- enigmatic.js
+++ enigmatic.js
@@ -1,5 +1,4 @@
 enigmatic = async x => {
-<<<<<<< HEAD
     try {
       window.helloworld = e =>
         e.innerHTML = 'Hello World!'
@@ -15,110 +14,47 @@ enigmatic = async x => {
              if(document.scripts[i].src == s) return r()
          }
          var e = document.createElement(iscss ? 'link' : 'script')
-         if(iscss) e.rel = "stylesheet";
-         e[iscss ? 'href' : 'src'] = s; 
+         if(iscss) e.rel = "stylesheet"
+         e[iscss ? 'href' : 'src'] = s
          document.body.appendChild(e)
-         e.onload = x => {
-           console.log(s); r(s);
-         }
+         e.onload = r
        })
-=======
- try {
-   window.helloworld = e =>
-     e.innerHTML = 'Hello World!'
-   window.views = e =>
-    e.set = showid =>
-     $('views > view').forEach( v => v.hidden = v.id != showid )
-  
-   window.load = s => {
-    return new Promise(r => {
-      var iscss = s.match(/css$/);
-      if(!iscss){
-        for(i=0;i<document.scripts.length;i++)
-          if(document.scripts[i].src == s) return r()
-      }
-      var e = document.createElement(iscss ? 'link' : 'script')
-      if(iscss) e.rel = "stylesheet";
-      e[iscss ? 'href' : 'src'] = s; 
-      document.body.appendChild(e)
-      e.onload = x => {
-        console.log(s); r(s);
->>>>>>> cc2998ef92e56ae9d917768befdba0c44b0752f5
-      }
-      window.$ = document.querySelectorAll.bind(document);
+      window.$ = document.querySelectorAll.bind(document)
       window.data = new Proxy({}, {
        set: (target, property, value, receiver) => {
          var arr = $(`[data=${property}]`)
          if(arr){
            arr.forEach(e => {
-            console.log(`set data ${e.tagName} ${JSON.stringify(value)}`);
-            if(e.set) e.set(value);
+            console.warn(`set data ${e.tagName} ${JSON.stringify(value)}`)
+            if(e.set) e.set(value)
            });
          }
-         target[property] = value;
+         target[property] = value
        }
-      });
-      $('[control]').forEach(e => {
-       console.log(e) 
-       var ename = e.tagName.toLowerCase();
-       var cn = e.getAttribute('control');
-       if(cn) ename = cn;
+      })
+
+      let e = $('[control]')
+      for(let i=0; i<e.length; i++){
+       console.warn(e)
+       let name = e.getAttribute('control') || e.tagName.toLowerCase()
        e.css = (rules, sel)=>{
-         var style = document.createElement("style");
-         document.head.appendChild(style);
-         style.sheet.insertRule(`${sel||e.tagName} { ${rules} }`); 
+         var style = document.createElement("style")
+         document.head.appendChild(style)
+         style.sheet.insertRule(`${sel||name} { ${rules} }`)
        }
-       await window[ename](e)
-      }); 
-      window.get = async url => {
-        var res = await fetch(url)
-        var d = await res.json()
-        return d;      
+       await window[name](e)
       }
-<<<<<<< HEAD
+
       (async () => {
-         var dataurl = $('meta[data]')[0];
+         let dataurl = $('meta[data]')[0];
          if(dataurl){
-           var d = await get(dataurl.getAttribute('data'))
+           let d = await get(dataurl.getAttribute('data'))
            for(k in d) data[k] = d[k]
          }
       })()
-      console.log('enigmatic runtime')
+      console.warn('enigmatic runtime')
     } catch(e) {
       document.write(e.stack)
-    }  
+    } 
    }
-=======
-      target[property] = value;
-    }
-   });
-   $('[control]').forEach(e => {
-    console.log(e) 
-    var ename = e.tagName.toLowerCase();
-    var cn = e.getAttribute('control');
-    if(cn) ename = cn;
-    e.css = (rules, sel)=>{
-      var style = document.createElement("style");
-      document.head.appendChild(style);
-      style.sheet.insertRule(`${sel||e.tagName} { ${rules} }`); 
-    }
-    await window[ename](e)
-   }); 
-   window.get = async url => {
-     var res = await fetch(url)
-     var d = await res.json()
-     return d;      
-   }
-   (async () => {
-      var dataurl = $('meta[data]')[0];
-      if(dataurl){
-        var d = await get(dataurl.getAttribute('data'))
-        for(k in d) data[k] = d[k]
-      }
-   })()
-   console.log('enigmatic runtime')
- } catch(e) {
-   document.write(e.stack)
- }  
-}
->>>>>>> cc2998ef92e56ae9d917768befdba0c44b0752f5
+  }
