76f32f3752d5f6341bfbf730788250db80b4b7ac
digital planets <digplan@users.noreply.github.com>
2017-11-27 18:07:56 -0600

    Update enigmatic.js

--- enigmatic.js
+++ enigmatic.js
@@ -1,10 +1,5 @@
 enigmatic = async x => {
-  window.helloworld = e => e.innerHTML = 'Hello World!'
-  window.views = e => e.set = showid => $('views > view').forEach( v => v.hidden = v.id != showid )
-  window.gmap = async e => {
-    await load('http://maps.googleapis.com/maps/api/js?v=3')
-    new google.maps.Map(e, {center: new google.maps.LatLng(30, -80)})
-  }
+  window.controls = {helloworld: e => e.innerHTML='Hello World!'}
   window.$ = document.querySelectorAll.bind(document)
   window.load = s => {
     return new Promise(r => {
@@ -16,21 +11,17 @@ enigmatic = async x => {
       var e = document.createElement(iscss ? 'link' : 'script')
       if(iscss) e.rel = "stylesheet"
       e[iscss ? 'href' : 'src'] = s
-      console.warn(s)
       document.body.appendChild(e)
       e.onload = r
     })
   }
   window.data = new Proxy({}, {
     set: (target, property, value, receiver) => {
-      var arr = $(`[data=${property}]`)
-      if(arr){
-         arr.forEach(e => {
-         console.warn(`set data ${e.tagName} ${JSON.stringify(value)}`)
-         if(e.set) e.set(value)
-         });
-      }
-      target[property] = value
+      let arr = $(`[data=${property}]`)
+      if(!arr) return;
+      arr.forEach(e => {
+         if(e.set) e.set(value); else e.innerHTML = value
+      });
     }
   })
           
@@ -43,11 +34,11 @@ enigmatic = async x => {
      document.head.appendChild(style)
      style.sheet.insertRule(`${sel||name} { ${rules} }`)
     }
-    await window[name](e)
+    await window.controls[name](e)
   }
   let dataurl = $('meta[data]')[0];
   if(dataurl){
-    let d = await get(dataurl.getAttribute('data'))
+    let d = await (await fetch(dataurl.getAttribute('data'))).json()
     for(k in d) data[k] = d[k]
   }
 }
