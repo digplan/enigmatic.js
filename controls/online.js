71dcaf3a211a278aedc074e2a6d9f1cda777103f
digital planets <digplan@users.noreply.github.com>
2017-11-27 20:42:40 -0600

    Delete online.js

--- controls/online.js
+++
@@ -1,9 +0,0 @@
-// <online style='position:absolute' class='three two-high' control>
-
-enig.online = (e)=>{
- var check = k =>
-   e.style.backgroundColor = navigator.onLine ? 'green': '#f44242';
- e.innerHTML = '&nbsp';
- check();
- setInterval(check, 3000);
-}
