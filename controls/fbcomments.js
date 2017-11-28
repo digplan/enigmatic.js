3a23c0aef1248d0cca8a7775778bc1333f49f703
digital planets <digplan@users.noreply.github.com>
2017-11-27 20:57:28 -0600

    Delete fbcomments.js

--- controls/fbcomments.js
+++
@@ -1,4 +0,0 @@
-enig.fbcomments = (e) => {
-  e.innerHTML = `<div class="fb-comments" data-href="${location.href}" data-numposts="2"></div>`;
-  enig.load(`//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=${e.getAttribute('appid')}`);
-};
