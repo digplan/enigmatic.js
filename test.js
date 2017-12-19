448f94473156478d0918bddcabdd2322db59683c
digital planets <digplan@users.noreply.github.com>
2017-12-18 19:38:30 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 (async ()=>{
   const page = await require('testchrome')('https://digplan.github.io/test.html')
   const html = page.evaluate('document.documentElement.outerHTML')
-
+  console.log(html)
+  
   process.exit(0) 
 })().catch(e=>{console.error(e); process.exit(1)})
